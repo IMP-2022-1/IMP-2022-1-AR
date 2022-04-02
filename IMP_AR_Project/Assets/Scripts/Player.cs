1b02bc97b11be6e93f51fd737ac431d8100b0cf0
PAVDUP <hwi7586@ajou.ac.kr>
2022-04-02 19:41:17 +0900

    Restore

---
+++ IMP_AR_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class Player : MonoBehaviour
+{
+    public int HP = 3;
+
+
+    [SerializeField] private Camera arCamera;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray;
+            RaycastHit hit;
+
+            ray = arCamera.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider != null && hit.transform.gameObject.CompareTag("Mosquito"))
+            {
+                Destroy(hit.transform.gameObject);
+                GameManager.instance.Score++;
+                GameManager.instance.TimeLimit = 10f;
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Rotate(-3f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Rotate(3f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, -3f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(0, 3f, 0);
+        }
+
+    }
+}
---
+++ IMP_AR_Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public bool spawnEnable;
+
+    public GameObject Mosquito;
+
+    //스폰할 모기 레벨 체크할 인스턴스
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnMosquito();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject mosq = GameObject.FindGameObjectWithTag("Mosquito");
+
+        if (mosq == null)
+        {
+            Debug.Log("1");
+            spawnEnable = true;
+        }
+        else
+            spawnEnable = false;
+
+        if (spawnEnable)
+        {
+            spawnMosquito();
+        }
+    }
+
+    void spawnMosquito()
+    {
+        float randomX = Random.Range(-5f, 5f);
+        float randomZ = Random.Range(-5f, 5f);
+
+        GameObject mosq = (GameObject)Instantiate(Mosquito, new Vector3(randomX, 0f, randomZ), Quaternion.identity);
+    } 
+}
