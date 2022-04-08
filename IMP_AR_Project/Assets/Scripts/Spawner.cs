29ad93c7b63980078a18d8665ccfe38f53ae447d
Jbrochan <hyeongchanj@gamil.com>
2022-04-08 10:59:32 +0900

    Make destroyMousquito()

--- IMP_AR_Project/Assets/Scripts/Spawner.cs
+++ IMP_AR_Project/Assets/Scripts/Spawner.cs
@@ -8,18 +8,27 @@ public class Spawner : MonoBehaviour
 
     public GameObject Mosquito;
 
+    // Variable for spawnMosquito();
+    GameObject mosq;
+
+
+
+    // Variable for using Timelimit in destroyMosquito()
+    GameManager gameManager;
+
     //스폰할 모기 레벨 체크할 인스턴스
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("GamerManager").GetComponent<GameManager>();
         spawnMosquito();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject mosq = GameObject.FindGameObjectWithTag("Mosquito");
+        mosq = GameObject.FindGameObjectWithTag("Mosquito");
 
         if (mosq == null)
         {
@@ -40,6 +49,18 @@ public class Spawner : MonoBehaviour
         float randomX = Random.Range(-5f, 5f);
         float randomZ = Random.Range(-5f, 5f);
 
-        GameObject mosq = (GameObject)Instantiate(Mosquito, new Vector3(randomX, 0f, randomZ), Quaternion.identity);
-    } 
+        mosq = (GameObject)Instantiate(Mosquito, new Vector3(randomX, 0f, randomZ), Quaternion.identity);
+    }
+    
+    void destroyMousquito()
+    {
+        // Destroy mosquito if time limit is over
+        if(gameManager.TimeLimit == 0)
+        {
+            Destroy(mosq);
+        }
+
+        // Destroy mosquito if mosquito's HP is 0
+        // Mosquito_HP in mosquito.cs is gone
+    }
 }
