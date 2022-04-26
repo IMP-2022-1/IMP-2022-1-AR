4cdaccb1022c774748bd631318b9fb988882a4fb
Jbrochan <hyeongchanj@gamil.com>
2022-04-27 02:21:55 +0900

    Update #3 :: Spawn potion at middle of first ARPlane, change prefab sphere to asset

--- IMP_AR_Project/Assets/Scripts/PlanePotion.cs
+++ IMP_AR_Project/Assets/Scripts/PlanePotion.cs
@@ -16,6 +16,7 @@ public class PlanePotion : MonoBehaviour
     [SerializeField]
     private GameObject realPotionToInstantiate;
 
+    public List<GameObject> ARPlanes = new List<GameObject>();
     private GameObject unrealPotion;
     private GameObject realPotion;
     
@@ -83,7 +84,7 @@ public class PlanePotion : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-       if(GameManager.instance.Score >= 3)
+       if(GameManager.instance.Score >= 3 && potionCheck)
         {
             spawnRealPotion();
         }
@@ -93,7 +94,7 @@ public class PlanePotion : MonoBehaviour
     // spawn unreal potion based on plane
     private void spawnUnRealPotion(ARPlane plane)
     {
-        // Random spawn in world space
+        /*// Random spawn in world space
         if (GameManager.instance.Score >= 3 && potionCheck)
         {
             Debug.Log("unrealPotion Spawn!!!");
@@ -101,37 +102,27 @@ public class PlanePotion : MonoBehaviour
             unrealPotion = Instantiate(unrealPotionToInstantiate, new Vector3(potionPosition.x, 0, potionPosition.z), Quaternion.identity);
             potionCheck = false;
 
-        }
+        }*/
 
         // Spawn in plane's center
-        /*if (GameManager.instance.Score >= 3 && potionCheck)
+        if (GameManager.instance.Score >= 3 && potionCheck)
         {
             Debug.Log("unrealPotion Spawn!!!");
-            Vector3 potionPosition = Random.insideUnitCircle.normalized;
-            unrealPotion = Instantiate(unrealPotionToInstantiate, plane.center, Quaternion.identity);
+            unrealPotion = Instantiate(unrealPotionToInstantiate, plane.transform.position, Quaternion.identity);
             potionCheck = false;
-        }*/
+        }
     }
 
     // Spawn real potion when player get closed to unreal potion
     private void spawnRealPotion()
     {
-        // Random spawn in world space
-        /*
-        Vector3 potionPosition = Random.insideUnitCircle.normalized;
-        potion = Instantiate(potionToInstantiate, new Vector3(potionPosition.x, 0, potionPosition.z), Quaternion.identity);
-        */
-
-
         // Spawn in plane's center
         Vector3 unrealPotionPosition = unrealPotion.transform.position;
-        if (Vector3.Distance(ARCameraPosition, unrealPotionPosition)<0.2f)
+        if (Vector3.Distance(ARCameraPosition, unrealPotionPosition)<1f)
         {
             Debug.Log("realPotion Spawn!!!");
-            Vector3 potionPosition = Random.insideUnitCircle.normalized;
             realPotion = Instantiate(realPotionToInstantiate, unrealPotionPosition, Quaternion.identity);
             Destroy(unrealPotion);
-            potionCheck = false;
         }
     }
 }
